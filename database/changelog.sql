0f780930077f9130d7ba54c41f018ff74ca4bf0b
sae <sae@>
2025-11-19 12:39:04 +0100

    update

--- database/changelog.sql
+++ database/changelog.sql
@@ -1,8 +1,4 @@
 --liquibase formatted sql
 
---changeset usersae301:001
-CREATE TABLE IF NOT EXISTS demo_table (
-    id SERIAL PRIMARY KEY,
-    label TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
+--changeset usersae301:002
+
