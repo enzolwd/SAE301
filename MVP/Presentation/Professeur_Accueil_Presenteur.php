c4bdf36c29adc1b5f68467a30b9d36046a0fb764
antoi <antoineplichon2@gmail.com>
2025-11-27 16:09:40 +0100

    correction petit soucis côté prof

--- MVP/Presentation/Professeur_Accueil_Presenteur.php
+++ MVP/Presentation/Professeur_Accueil_Presenteur.php
@@ -12,22 +12,20 @@ require_once '../Modele/ConnexionBDD.php';
 require_once '../Modele/Professeur_Modele.php';
 
 // vérifier si l'utilisateur s'est connecté
-if (!isset($_SESSION['idUtilisateur']) || $_SESSION['role'] != 'Professeur') { // Sécurité rôle
-    header('Location: ../Vue/Page_de_connexion/Page_De_Connexion.php');
+if (!isset($_SESSION['idUtilisateur']) || $_SESSION['role'] != 'Professeur') {
+    header('Location: ../Vue/Page_De_Connexion.php');
     exit();
 }
 
 $idProf = $_SESSION['idUtilisateur'];
 
-// 1. On crée la connexion
 $conn1 = connecterBDD();
 
 try {
-    // 2. On demande les données au Modele
     $lesRattrapages = recupererRattrapagesProf($conn1, $idProf);
 
-} catch(Exception $e) { // Changé de PDOException
-    header('Location: ../Vue/Page_de_connexion/Page_De_Connexion.php');
+} catch(Exception $e) {
+    header('Location: ../Vue/Page_De_Connexion.php');
     exit();
 }
 
